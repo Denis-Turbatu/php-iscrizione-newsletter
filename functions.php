a197bfa7c9f087cc13db6588fc59041a45dcf4e7
Denis-Turbatu <turbatudenis81@gmail.com>
2024-05-23 09:52:01 +0200

    feat: session on functions.php and thankyou.php

--- functions.php
+++ functions.php
@@ -1,10 +1,18 @@
 <?php
 $ver_email = $_GET["email"];
 
+session_start();
+
 if ($ver_email === null) {
     echo "";
 } elseif (str_contains($ver_email, "@") && str_contains($ver_email, ".")) {
+    $_SESSION["auth"] = true;
     header('Location: ./thankyou.php');
 } else { ?>
     <div class="alert alert-danger">L'email non soddisfa i requisiti minimi: Aggiungi '@' e '.'</div>
-<?php } ?>
+<?php } 
+
+// session_destroy();
+// header("Location: index.php");
+
+?>
--- thankyou.php
+++ thankyou.php
@@ -1,10 +1,12 @@
 <?php
     include "./template/head.php";
     include "./functions.php";
-?>
 
-    <div class="alert alert-success">L'email soddisfa i requisiti minimi :)</div>
-
+    if (isset($_SESSION["auth"]) || $_SESSION["auth"] === true) { ?>
+        <div class="alert alert-success">L'email soddisfa i requisiti minimi :)</div>
+    <?php }else{?>
+        <h2>C'è un problema</h2>
+    <?php } ?>
 <?php
     include "./template/body.php";
 ?>
